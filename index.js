f61da89a9a12f8c8f483b14213d86f89bcb57bb4
Victoria B <viktorija.bourassa@gmail.com>
2019-01-14 00:18:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue){
-  for (let i = 0; i < array.length; i++){
     newA = [];
+  for (let i = 0; i < array.length; i++){
     if (array[i] !== stopValue){
       array[i] = changeValue;
       newA.push(array[i]);
